98f57721bce3d3f5c0fce7f4dd57e06789671663
Antonia Hadjimichael <ah986@cornell.edu>
2020-02-16 21:32:57 -0500

    loop through factors

--- deltaSA.py
+++ deltaSA.py
@@ -2,7 +2,9 @@ import numpy as np
 from SALib.analyze import delta
 from SALib.sample import latin
 from fishery import fish_game
+import matplotlib.pyplot as plt
 
+# Set up dictionary with system parameters
 problem = {
   'num_vars': 9,
   'names': ['a', 'b', 'c', 'd','h',
@@ -19,12 +21,39 @@ problem = {
 }
 
 # Generate samples
-X = latin.sample(problem, 1000)
+nsamples = 1000
+X = latin.sample(problem, nsamples)
 
-# Run model (example)
-Y = [list(fish_game(*X[i,:])) for i in range(len(X))]
+# Run model for all samples
+output = [list(fish_game(*X[j,:])) for j in range(nsamples)]
 
-Y_NPV=np.asarray(list(zip(*Y))[0])
+# Pick out objective of interest
+Y=np.asarray(list(zip(*output))[0])
 
 # Perform analysis
-results = delta.analyze(problem, X, Y_NPV, print_to_console=True)
+results = delta.analyze(problem, X, Y, print_to_console=True)
+
+# Sort factors by importance
+factors_sorted = np.argsort(results['delta'])[::-1]
+
+for f in range(1, len(factors_sorted)):
+    ntopfactors = f
+    problem_reduced = {'num_vars': ntopfactors,
+                       'names': [problem['names'][factors_sorted[i]] for i in range(ntopfactors)],
+                       'bounds': [problem['bounds'][factors_sorted[i]] for i in range(ntopfactors)]}
+    
+    # Generate samples
+    X_reduced = latin.sample(problem_reduced, nsamples)
+    
+    # Run model for all samples
+    output_reduced = [[]]*nsamples
+    for j in range (nsamples):
+        kwargs = {problem['names'][factors_sorted[i]]:X_reduced[j,i] for i in range(ntopfactors)}
+        output_reduced[j] = list(fish_game(**kwargs))
+    
+    # Pick out objective of interest
+    Y_reduced=np.asarray(list(zip(*output_reduced))[0])
+    plt.scatter(np.sort(Y),np.sort(Y_reduced))
+    plt.plot(np.sort(Y),np.sort(Y))
+    if np.corrcoef(np.sort(Y),np.sort(Y_reduced))[0][1] >= 0.9:
+      break
